4780a061c4222727659607d840a75dd140acc42b
Alemi Herbert <alemiherbert@gmail.com>
2023-07-22 16:40:05 +0300

    Prints the command to the terminal

---
+++ shell.c
@@ -0,0 +1,28 @@
+#include "shell.h"
+
+
+/**
+ * main - a command line executor
+ * @argc: the argument count
+ * @argv: the argument vector
+ * Return: Always 0
+ */
+int main(void)
+{
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t nread;
+	bool status = true;
+
+	do
+	{
+		printf("$ ");
+		nread = getline(&line, &len, stdin);
+
+		if (nread == EOF)
+			status = false;
+		
+		printf("%s", line);
+	} while (status == true);
+	return (0);
+}
