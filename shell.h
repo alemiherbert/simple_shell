3afbc98c7398da9e7d892b12d2ec776780251ba7
Alemi Herbert <alemiherbert@gmail.com>
2023-07-24 23:58:59 +0300

    Made several improvements to the tokeniser

--- shell.h
+++ shell.h
@@ -6,12 +6,14 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdbool.h>
-
+#include <sys/stat.h>
 
 #define DELIM " \a\t\n"
 #define MAX_TOKEN_SIZE 512
 #define MAX_BUFFER_SIZE 1024
 
 char **tokenise(char *command, char *delim);
+char *join_path(const char *dir, const char *file);
+char *locate_executable(const char *filename);
 
 #endif
--- utilities.c
+++ utilities.c
@@ -4,32 +4,52 @@
  * tokenise - split a string into tokens
  * @command: the string
  * @delim: the delimiter
- * 
+ *
  * Return: a pointer to the tokens or NULL
  */
 char **tokenise(char *command, char *delim)
 {
-	char *token, **tokens, *temp_command;
+	char *token = NULL, **tokens, *temp_command;
 	size_t i = 0;
 
 	if (command == NULL)
-		return (NULL);
+		return NULL;
 
 	tokens = malloc(sizeof(char *) * MAX_TOKEN_SIZE);
 	if (tokens == NULL)
-		return (NULL);
+		return NULL;
 
 	/* work with a copy of the command to avoid modifying it */
 	temp_command = strdup(command);
 
-	do {
-		if (token == NULL)
-			token = strtok(command, delim);
-		else
-			token = strtok(NULL, delim);
-		tokens[i++] = token;
-	} while (token != NULL && i < MAX_TOKEN_SIZE);
+	token = strtok(temp_command, delim);
+	while (token != NULL && i < MAX_TOKEN_SIZE)
+	{
+		tokens[i] = strdup(token);
+		token = strtok(NULL, delim);
+		i++;
+	}
 
 	free(temp_command);
-	return (tokens);
+	return tokens;
+}
+
+int main(void)
+{
+	// testing
+	char *command = "This is a sample command";
+	char **tokens = tokenise(command, DELIM);
+
+	if (tokens != NULL)
+	{
+		for (size_t i = 0; tokens[i] != NULL; i++)
+		{
+			printf("Token %zu: %s\n", i, tokens[i]);
+		}
+
+		// Free memory allocated for tokens
+		free(tokens);
+	}
+
+	return (0);
 }
